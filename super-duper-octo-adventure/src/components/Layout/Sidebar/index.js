158d6e973ebccbdcac5df0ca6b3835acbcc3253b
Nita Roberts <nitaroberts05@gmail.com>
2022-05-24 20:24:20 -0700

    added navbar

--- super-duper-octo-adventure/src/components/Layout/Sidebar/index.js
+++ super-duper-octo-adventure/src/components/Layout/Sidebar/index.js
@@ -1,12 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoN from '../../assets/images/logo-n.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
   <div className="nav-bar">
-    <Link className="logo" to="/"></Link>
-    <img src={LogoS} alt="logo" />
-    <img src={LogoS} alt="logo" />
+    <Link className="logo" to="/">
+      <img src={LogoN} alt="logo" />
+      <img src={LogoSubtitle} alt="nita" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 )
 
