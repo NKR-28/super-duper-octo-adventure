b97b7f43752ee8fd1a0269360bad06901193cf76
Nita Roberts <nitaroberts05@gmail.com>
2022-05-30 15:34:11 -0700

    added animation

--- super-duper-octo-adventure/src/components/Layout/Home/index.js
+++ super-duper-octo-adventure/src/components/Layout/Home/index.js
@@ -1,8 +1,27 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-n.png'
 import './index.scss'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 't', 'a']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
   return (
     <div className="container home-page">
       <div className="text-zone">
