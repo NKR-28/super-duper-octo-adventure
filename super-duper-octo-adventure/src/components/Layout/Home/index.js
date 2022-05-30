5a35c201136e43b425da15ea07fcaa2fed45db70
Nita Roberts <nitaroberts05@gmail.com>
2022-05-30 15:13:03 -0700

    styling homepage

--- super-duper-octo-adventure/src/components/Layout/Home/index.js
+++ super-duper-octo-adventure/src/components/Layout/Home/index.js
@@ -1,3 +1,26 @@
-const Home = () => 'Hello'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-n.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          {' '}
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          ita
+          <br />
+          web developer
+        </h1>
+        <h2>Full Stack Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
 
 export default Home
