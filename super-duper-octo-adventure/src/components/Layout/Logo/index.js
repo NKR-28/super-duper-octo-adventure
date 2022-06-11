02bd76e04d5f92777123f9946e4d1a1c7962e1d7
Nita Roberts <nitaroberts05@gmail.com>
2022-06-11 13:49:54 -0700

    testing

---
+++ super-duper-octo-adventure/src/components/Layout/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import LogoS from '../../../assets/images/logo-n.png'
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img className="solid-logo" />
+    </div>
+  )
+}
+
+export default Logo
+
+// testing testing 123
